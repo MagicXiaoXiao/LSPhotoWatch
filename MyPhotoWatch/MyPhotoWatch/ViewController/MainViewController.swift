80ecfbe4f225f7645825b6282da78249af74be2e
晓晓魔导师 <1171856045@qq.com>
2017-04-14 18:48:02 +0800

    导入Hero第三方库

--- MyPhotoWatch/MyPhotoWatch/ViewController/MainViewController.swift
+++ MyPhotoWatch/MyPhotoWatch/ViewController/MainViewController.swift
@@ -35,7 +35,8 @@ class MainViewController: UIViewController {
                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492167166551&di=ce905c6efd3ccc4403ce8e1cb2f9fb7d&imgtype=0&src=http%3A%2F%2Fi3.hexunimg.cn%2F2016-09-28%2F186224738.jpg",
                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492167200707&di=fffa5fb731fbe265dae9733f2c491528&imgtype=0&src=http%3A%2F%2Fh6.86.cc%2Fwalls%2F20160721%2Fmid_bdaee2a345fd80d.jpg",
                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492167219159&di=e408ea967b2769538ad7e3c6bacf51e6&imgtype=0&src=http%3A%2F%2Fh5.86.cc%2Fwalls%2F20160708%2Fmid_eb33900cc93f31e.jpg",
-                     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492167255337&di=1ddfdb1dafd69109c41ac3636756fc68&imgtype=0&src=http%3A%2F%2Fv1.qzone.cc%2Fskin%2F201608%2F27%2F09%2F05%2F57c0e747bc465901.png%2521600x600.jpg"]
+                     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492167255337&di=1ddfdb1dafd69109c41ac3636756fc68&imgtype=0&src=http%3A%2F%2Fv1.qzone.cc%2Fskin%2F201608%2F27%2F09%2F05%2F57c0e747bc465901.png%2521600x600.jpg",
+                     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492174593180&di=d745fc5ea6ed4ab89f9573437cbaeeff&imgtype=0&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2014%2F064%2F81G4I26G152L_1000x500.jpg"]
     
     
     override func viewDidLoad() {
@@ -77,21 +78,27 @@ extension MainViewController: UICollectionViewDataSource {
 }
 
 extension MainViewController: UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
         if let cell = cell as? SmallPhotoCell {
             cell.mainImageView.kf.setImage(
                 with: URL(string: testArray[indexPath.row]),
                 placeholder: nil,
                 options: [.transition(.fade(1))],
                 progressBlock: nil,
-                completionHandler: nil)
+                completionHandler: { (image, error, cache, url) in
+                    
+            })
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         guard let cell = cell as? SmallPhotoCell else { return }
         cell.mainImageView.kf.cancelDownloadTask()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toBigPhoto", sender: nil)
     }
     
 }
